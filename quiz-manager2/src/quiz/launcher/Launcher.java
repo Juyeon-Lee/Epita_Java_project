553cb040796f8993cdb4ade8d74f4cb05f0ee842
Juyeon-Lee <ljo0831@naver.com>
2019-02-10 17:30:23 +0100

    console - admin quesions - insert (delete merge error)

--- quiz-manager2/src/quiz/launcher/Launcher.java
+++ quiz-manager2/src/quiz/launcher/Launcher.java
@@ -27,36 +27,36 @@ public class Launcher {
 
 	 */
 	public static void main(String[] args) {
-        // TODO Auto-generated method st
-        Scanner scanner = new Scanner(System.in);
-        String answer;
-        boolean continueAddition = true;
+		Scanner scanner = new Scanner(System.in);
+		String answer;
+		boolean continueAddition = true;
 
-        System.out.println("Quiz Manager");
-        System.out.println("-----------------------");
-        while (continueAddition) {
-            System.out.println("Please enter your type");
-            System.out.println("1. admin");
-            System.out.println("2. student(solve a quiz)");
-            answer = scanner.nextLine();
+		System.out.println("Quiz Manager");
+		System.out.println("-----------------------");
+		while (continueAddition) {
+			System.out.println("Please enter your type");
+			System.out.println("1. admin");
+			System.out.println("2. student(solve a quiz)");
+			answer = scanner.nextLine();
 
-            if (answer.equals("1")) {
+			if (answer.equals("1")) {
 
-            } else if (answer.equals("2")) {
-                System.out.println("Quiz Start");
-            } else {
-                System.out.println("You did not enter a valid answer!");
-            }
-            System.out.println("do you want to continue solving a quiz? (Y/N)");
-            answer = scanner.nextLine();
-            continueAddition = "Y".equals(answer); //answer.equals("Y");
-        }
+			} else if (answer.equals("2")) {
+				System.out.println("Quiz Start");
+			} else {
+				System.out.println("You did not enter a valid answer!");
+			}
+			System.out.println("do you want to continue solving a quiz? (Y/N)");
+			answer = scanner.nextLine();
+			continueAddition = "Y".equals(answer); //answer.equals("Y");
+		}
 
-		insertQuestion();
+		insertQuestion(scanner);
+
+		scanner.close();
 	}
 
-	private static void insertQuestion(){
-		Scanner scanner = new Scanner(System.in);
+	private static void insertQuestion(Scanner scanner){
 		System.out.println("******Insert Question******");
 		System.out.println("Do you want to insert a MCQQuestion? : y/n");
 		char mcq = scanner.nextLine().charAt(0);
@@ -92,19 +92,6 @@ public class Launcher {
 			s = scanner.nextLine();
 		}
 		//System.out.println(question);
-    public static void main(String[] args) {
-        // TODO Auto-generated method st
-        Scanner scanner = new Scanner(System.in);
-        String answer;
-        boolean continueAddition = true;
-
-        System.out.println("Quiz Manager");
-        System.out.println("-----------------------");
-        while (continueAddition) {
-            System.out.println("Please enter your type");
-            System.out.println("1. admin");
-            System.out.println("2. student(solve a quiz)");
-            answer = scanner.nextLine();
 
 		System.out.println("Topic : (Enter 'end' at the last line When you want stop typing for topics.)");
 		List<String> topics = new ArrayList<String>();
