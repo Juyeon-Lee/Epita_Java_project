6e191ca48d582490a879079b3aabf68ef7510dfd
Juyeon-Lee <ljo0831@naver.com>
2019-02-10 17:09:25 +0100

    console - admin quesions - insert

--- quiz-manager2/src/quiz/launcher/Launcher.java
+++ quiz-manager2/src/quiz/launcher/Launcher.java
@@ -1,10 +1,107 @@
 package quiz.launcher;
 
+import quiz.datamodel.MCQQuestion;
+import quiz.datamodel.Question;
+import quiz.services.MCQQuestionJDBCDAO;
+import quiz.services.QuestionJDBCDAO;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Launcher {
 
+	/*
+	■ admin questions
+-insert(create)
+-update
+-delete
+-show all(read)
+-show (search by topics)
+
+	■ Solve a quiz
+-insert student info
+ : solving a quiz > select Q randomly by topics > show score > export quiz?: Yes?no
+-Search(topic, difficulty)
+-show (search by topics)
+
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		insertQuestion();
 	}
 
+	private static void insertQuestion(){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("******Insert Question******");
+		System.out.println("Do you want to insert a MCQQuestion? : y/n");
+		char mcq = scanner.nextLine().charAt(0);
+
+		if(mcq=='y'){
+			Question question =  insertGeneralInfo(scanner);
+			List<String> mcqInfo = insertMCQInfo(scanner);
+
+			// insert MCQQuestion query
+			MCQQuestionJDBCDAO mcqDao = new MCQQuestionJDBCDAO();
+			int mcqId = mcqDao.create(mcqInfo);
+
+			// insert Question query
+			QuestionJDBCDAO dao = new QuestionJDBCDAO();
+			dao.create(question,mcqId);
+		}
+		else{
+			Question question = insertGeneralInfo(scanner);
+
+			// insert Question query
+			QuestionJDBCDAO dao = new QuestionJDBCDAO();
+			dao.create(question, 0);
+		}
+	}
+
+	private static Question insertGeneralInfo(Scanner scanner) {
+		System.out.println("Question contents: (Enter 'end' at the last line When you want stop typing for question.)");
+		String s = scanner.nextLine();
+		String question="";
+		while(!(s.equals("end")))
+		{
+			question = question.concat(s).concat("\n");
+			s = scanner.nextLine();
+		}
+		//System.out.println(question);
+
+		System.out.println("Topic : (Enter 'end' at the last line When you want stop typing for topics.)");
+		List<String> topics = new ArrayList<String>();
+
+		do{
+			s = scanner.nextLine();
+			topics.add("s");
+		}while(!(s.equals("end")));
+
+		System.out.println("Difficulty : ");
+		int difficulty = scanner.nextInt();
+		scanner.nextLine();
+
+		Question q = new Question(question, topics, difficulty);
+		return q;
+	}
+	private static List<String> insertMCQInfo(Scanner scanner){
+		List<String> s = new ArrayList<String>();
+
+		System.out.println("choice1 : ");
+		s.add(scanner.nextLine());
+
+		System.out.println("choice2 : ");
+		s.add(scanner.nextLine());
+
+		System.out.println("choice3 : ");
+		s.add(scanner.nextLine());
+
+		System.out.println("choice4 : ");
+		s.add(scanner.nextLine());
+
+		System.out.println("Answer : ");
+		s.add(scanner.nextLine());
+
+		return s;
+	}
 }
--- quiz-manager2/src/quiz/services/MCQQuestionJDBCDAO.java
+++ quiz-manager2/src/quiz/services/MCQQuestionJDBCDAO.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import quiz.datamodel.MCQQuestion;
 import quiz.datamodel.Question;
@@ -22,25 +23,44 @@ select * from question;
 	
    private static final String INSERT_STATEMENT = "INSERT INTO MCQQUESTION (CHOICE1, CHOICE2, CHOICE3, CHOICE4, ANSWER) VALUES (?, ?, ?, ?, ?)";
    private static final String SEARCH_STATEMENT = "SELECT * FROM MCQQUESTION";
-   private static final String UPDATE_STATEMENT = "UPDATE MCQQUESTION SET CHOICE1=?, CHOICE2=?, CHOICE3=?, CHOICE4=?, ANSWER=? WHERE ID=?";
+   private static final String UPDATE_STATEMENT = "UPDATE MCQQUESTION SET CHOICE1=?, CHOICE2=?, CHOICE3=?, CHOICE4=?, ANSWER=? WHERE MCQID=?";
    private static final String DELETE_STATEMENT = "DELETE FROM MCQQUESTION WHERE MCQID = ?";
+
+	private final static Logger LOG = Logger.getGlobal();
 	
-	
-	
-	public void create(MCQQuestion MCQ) {
+	public int create(List<String> mcqInfo) {
 		
 		try (Connection connection = getConnection();
 				PreparedStatement insertStatement = connection.prepareStatement(INSERT_STATEMENT);) {
 			
-			insertStatement.setString(1, MCQ.getQuestion());
-			insertStatement.setInt(2, MCQ.getDifficulty());
+			insertStatement.setString(1, mcqInfo.get(0)); //choice1
+			insertStatement.setString(2, mcqInfo.get(1)); //choice2
+			insertStatement.setString(3, mcqInfo.get(2)); //choice3
+			insertStatement.setString(4, mcqInfo.get(3)); //choice4
+			insertStatement.setString(5, mcqInfo.get(4)); //answer
 			
 			insertStatement.execute();
+			LOG.info("insert success");
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 
+		String selectQuery = "select * from MCQQUESTION " +
+				"WHERE {choice1=mcqInfo.get(0) and choice2=mcqInfo.get(1) " +
+				"and choice3=mcqInfo.get(2) and choice4=mcqInfo.get(3) and answer=mcqInfo.get(4)}";
+		try (Connection connection = getConnection();
+			 PreparedStatement preparedStatement = connection.prepareStatement(selectQuery);)
+		{
+			ResultSet results = preparedStatement.executeQuery();
+			int mcqId = results.getInt(0);
+			LOG.info("done getting MCQId : "+mcqId);
+			results.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return 0;// 0 : error
 	}
 
 	public void update(MCQQuestion MCQ) {
